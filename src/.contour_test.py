e7af111b320a602e5ba4b6fbf45a3803131b6a86
= <nathanieltanglin@gmail.com>
2025-11-21 16:04:18 -0600

    Removed plots. Cleared jupyter notebook outputs.

--- src/.contour_test.py
+++
@@ -1,4 +0,0 @@
-from lib import *
-
-#build_contour_VPLanet(save_name = 'earth_trappist_central_HZ.png', directory = os.path.join(os.pardir, 'data', 'Earth_Trappist_Central_HZ'), planet_file_name = 'trappist.earth.forward', mode = ATM | PERCENT)
-build_contour_VPLanet(save_name = 'earth_kv_central_HZ.png', directory = os.path.join(os.pardir, 'data', 'Earth_KV_Central_HZ'), planet_file_name = 'kv.earth.forward', mode = ATM | PERCENT)
